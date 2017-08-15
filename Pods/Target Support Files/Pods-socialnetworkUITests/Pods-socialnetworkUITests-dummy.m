95022e8d768e10f139f30d0cbced50df8b295ac4
Flamur <flamurbeqiri1@gmail.com>
2017-08-15 14:18:52 +0200

    First commit

---
+++ Pods/Target Support Files/Pods-socialnetworkUITests/Pods-socialnetworkUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_socialnetworkUITests : NSObject
+@end
+@implementation PodsDummy_Pods_socialnetworkUITests
+@end
---
+++ socialnetwork/SignInVC.swift
@@ -0,0 +1,26 @@
+//
+//  SignInVC.swift
+//  socialnetwork
+//
+//  Created by Flamur on 8/15/17.
+//  Copyright © 2017 Codeators. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
